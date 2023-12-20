29cc7bdeb283f1f783f22766ba8d23f7c780d717
Pavel Pelovski <git.pelovski@gmail.com>
2023-12-20 22:04:58 +0100

    Initial commit

---
+++ Web/FCArsenalFanPage.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿namespace FCArsenalFanPage.Web.Areas.Administration.Controllers
+{
+    using FCArsenalFanPage.Services.Data;
+    using FCArsenalFanPage.Web.ViewModels.Administration.Dashboard;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class DashboardController : AdministrationController
+    {
+        private readonly ISettingsService settingsService;
+
+        public DashboardController(ISettingsService settingsService)
+        {
+            this.settingsService = settingsService;
+        }
+
+        public IActionResult Index()
+        {
+            var viewModel = new IndexViewModel { SettingsCount = this.settingsService.GetCount(), };
+            return this.View(viewModel);
+        }
+    }
+}
