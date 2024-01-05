c46b1e11dd3837b2dfdb73945b1082df42abc8ff
Pavel Pelovski <git.pelovski@gmail.com>
2024-01-05 20:35:33 +0100

    repair project

---
+++ Web/FCArsenalFanPage.Web/Controllers/NewsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FCArsenalFanPage.Web.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
