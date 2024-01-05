532daa4b6e5bad3b6c746a19c23aba1687015583
Pavel Pelovski <git.pelovski@gmail.com>
2024-01-05 20:35:33 +0100

    repair project

---
+++ Web/FCArsenalFanPage.Web/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FCArsenalFanPage.Web.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
