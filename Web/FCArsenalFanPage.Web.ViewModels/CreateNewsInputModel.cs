9804f6d49fad1f17e8627dfa9332d25fd945fefd
Pavel Pelovski <git.pelovski@gmail.com>
2024-01-07 16:02:52 +0100

    Added SingleNews view and action

--- Web/FCArsenalFanPage.Web.ViewModels/CreateNewsInputModel.cs
+++ Web/FCArsenalFanPage.Web.ViewModels/CreateNewsInputModel.cs
@@ -2,27 +2,30 @@
 {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    using FCArsenalFanPage.Data.Models;
+    using FCArsenalFanPage.Web.Infrastructure;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc.Rendering;
 
     public class CreateNewsInputModel
     {
-        [Required(ErrorMessage = "Title is required")]
-        [MinLength(10, ErrorMessage = $"{nameof(Title)} cannot be less than 10 characters")]
-        [MaxLength(100, ErrorMessage = $"{nameof(Title)} cannot exceed 100 characters")]
+        [Required(ErrorMessage = "Please enter a title.")]
+        [MinLength(10, ErrorMessage = $"{nameof(Title)} cannot be less than 10 characters.")]
+        [MaxLength(100, ErrorMessage = $"{nameof(Title)} cannot exceed 100 characters.")]
         public string Title { get; set; }
 
-        [Required(ErrorMessage = $"{nameof(Content)} is required")]
+        [MinLength(10, ErrorMessage = $"{nameof(Title)} cannot be less than 50 characters.")]
+        [Required(ErrorMessage = $"{nameof(Content)} is required.")]
         public string Content { get; set; }
 
-        [Required(ErrorMessage = "Categories are required")]
         [Display(Name = "Categories")]
         public int CategoryId { get; set; }
 
         public string CreatedByUserId { get; set; }
 
-        // TODO: Make custom validation attribute for extensions and size
+        [Required(ErrorMessage = "Please select an image.")]
+        [DataType(DataType.Upload)]
+        [ImageMaxFileSize(10 * 1024 * 1024)]
+        [ImageExtensionValidation(new string[] { ".jpg", ".gif", ".png" })]
         public IFormFile Image { get; set; }
 
         public IEnumerable<SelectListItem> CategoriesItems { get; set; }
