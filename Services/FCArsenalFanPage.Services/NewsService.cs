9be912f22eb6bb4d86ecb237613aa7850e845efe
Pavel Pelovski <git.pelovski@gmail.com>
2024-01-14 15:00:33 +0100

    news createAsync implementet

--- Services/FCArsenalFanPage.Services/NewsService.cs
+++ Services/FCArsenalFanPage.Services/NewsService.cs
@@ -2,11 +2,13 @@
 namespace FCArsenalFanPage.Services
 {
     using System;
+    using System.IO;
     using System.Threading.Tasks;
 
     using FCArsenalFanPage.Data.Common.Repositories;
     using FCArsenalFanPage.Data.Models;
     using FCArsenalFanPage.Web.ViewModels;
+    using Microsoft.AspNetCore.Http;
 
     public class NewsService : INewsService
     {
@@ -21,15 +23,37 @@ namespace FCArsenalFanPage.Services
             this.imageRepositoy = imageRepositoy;
         }
 
-        public Task CreateAsync(CreateNewsInputModel input, string userId, string imagePath)
+        public async Task CreateAsync(CreateNewsInputModel input, string userId, string imagePath)
         {
+            Directory.CreateDirectory($"{imagePath}/News/");
 
-            //Content
-            //CategoryId
-            //Title
+            var news = new News
+            {
+                Title = input.Title,
+                Content = input.Content,
+                CreatedByUserId = userId,
+                CategoryId = input.CategoryId,
+            };
 
+            var image = input.Image;
 
-            return Task.CompletedTask;
+            var imageExtension = Path.GetExtension(image.FileName).TrimStart('.');
+
+            news.Image = new Image
+            {
+                News = news,
+                Extension = imageExtension,
+            };
+
+            var imageId = news.Image.Id;
+
+            var physicalPath = $"{imagePath}/News/{imageId}.{imageExtension}";
+
+            using Stream fileStream = new FileStream(physicalPath, FileMode.Create);
+            await image.CopyToAsync(fileStream);
+
+            await this.newsRepository.AddAsync(news);
+            await this.newsRepository.SaveChangesAsync();
         }
     }
 }
