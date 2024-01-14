3fa6b2b86d9d0ce45f6e68476d6f6bd98772b459
Pavel Pelovski <git.pelovski@gmail.com>
2024-01-14 15:00:33 +0100

    news createAsync implementet

--- Web/FCArsenalFanPage.Web/Controllers/NewsController.cs
+++ Web/FCArsenalFanPage.Web/Controllers/NewsController.cs
@@ -3,11 +3,13 @@
     using System.Linq;
     using System.Security.Claims;
     using System.Threading.Tasks;
+
     using FCArsenalFanPage.Data.Common.Repositories;
     using FCArsenalFanPage.Data.Models;
     using FCArsenalFanPage.Services;
     using FCArsenalFanPage.Web.ViewModels;
     using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Mvc;
 
     public class NewsController : Controller
@@ -15,15 +17,18 @@
         private readonly ICategoriesService categoriesService;
         private readonly INewsService newsService;
         private readonly IDeletableEntityRepository<ApplicationUser> userRepository;
+        private readonly IHostingEnvironment environment;
 
         public NewsController(
             ICategoriesService categoriesService,
             INewsService newsService,
-            IDeletableEntityRepository<ApplicationUser> userRepository)
+            IDeletableEntityRepository<ApplicationUser> userRepository,
+            IHostingEnvironment environment)
         {
             this.categoriesService = categoriesService;
             this.newsService = newsService;
             this.userRepository = userRepository;
+            this.environment = environment;
         }
 
         public IActionResult All()
@@ -52,7 +57,9 @@
 
             var userId = this.User.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-            await this.newsService.CreateAsync(input, userId, "");
+            var imagePath = $"{this.environment.WebRootPath}/Images";
+
+            await this.newsService.CreateAsync(input, userId, imagePath);
 
             return this.RedirectToAction("All");
         }
