7ef567725c30c8238c7ad6de4d1abac25236bcbf
Pavel Pelovski <git.pelovski@gmail.com>
2024-02-14 15:23:13 +0100

    Scaffolded Administration and implementig it

--- Web/FCArsenalFanPage.Web/Program.cs
+++ Web/FCArsenalFanPage.Web/Program.cs
@@ -118,6 +118,7 @@
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.MapControllerRoute("areaRoute", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
         }
