64b3f6792e313d1f4b6d976913b49563d14a0f39
Pavel Pelovski <git.pelovski@gmail.com>
2024-03-09 11:11:31 +0100

    Removed unnecessary javascript

--- Web/FCArsenalFanPage.Web/Controllers/HomeController.cs
+++ Web/FCArsenalFanPage.Web/Controllers/HomeController.cs
@@ -1,23 +1,31 @@
 ﻿namespace FCArsenalFanPage.Web.Controllers
 {
     using System.Diagnostics;
-
+    using System.Security.Claims;
+    using FCArsenalFanPage.Data.Models;
     using FCArsenalFanPage.Services;
     using FCArsenalFanPage.Web.ViewModels;
     using FCArsenalFanPage.Web.ViewModels.Home;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
 
     public class HomeController : BaseController
     {
         private readonly INewsService newsService;
         private readonly IProductService productService;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IApplicationUserService userService;
 
         public HomeController(
             INewsService newsService,
-            IProductService productService)
+            IProductService productService,
+            UserManager<ApplicationUser> userManager,
+            IApplicationUserService userService)
         {
             this.newsService = newsService;
             this.productService = productService;
+            this.userManager = userManager;
+            this.userService = userService;
         }
 
         public IActionResult Index()
@@ -30,6 +38,18 @@
                 News = news,
                 Products = products,
             };
+
+            // Take the ImageUrl form the user to give it to the _LoginPartial
+            if (this.User.Identity.IsAuthenticated)
+            {
+                var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                var user = this.userManager.FindByIdAsync(userId).Result;
+
+                if (user.ProfilePictureId != null)
+                {
+                    this.ViewData["ImageUrl"] = this.userService.GetProfilePictureUrl(user);
+                }
+            }
 
             return this.View(viewModel);
         }
