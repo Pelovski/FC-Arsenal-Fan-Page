b0dea027e440781b45844d9743de70a4c81110d7
Pavel Pelovski <git.pelovski@gmail.com>
2024-03-19 19:35:19 +0100

    Implemented (http get only) set user role in admin panel

--- Web/FCArsenalFanPage.Web.ViewModels/Administration/EditUserRolesViewModel.cs
+++ Web/FCArsenalFanPage.Web.ViewModels/Administration/EditUserRolesViewModel.cs
@@ -6,6 +6,10 @@
 
     public class EditUserRolesViewModel
     {
+        public string Id { get; set; }
+
+        public string RoleId { get; set; }
+
         public IEnumerable<SelectListItem> Roles { get; set; }
     }
 }
--- Web/FCArsenalFanPage.Web/Areas/Administration/Controllers/UserRolesController.cs
+++ Web/FCArsenalFanPage.Web/Areas/Administration/Controllers/UserRolesController.cs
@@ -1,5 +1,6 @@
 ﻿namespace FCArsenalFanPage.Web.Areas.Administration.Controllers
 {
+    using System;
     using System.Linq;
     using System.Threading.Tasks;
     using FCArsenalFanPage.Data.Common.Repositories;
@@ -8,19 +9,24 @@
     using FCArsenalFanPage.Web.ViewModels.Administration;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+	using Microsoft.AspNetCore.Mvc.Rendering;
+	using Microsoft.Extensions.DependencyInjection;
 
     [Area("Administration")]
     public class UserRolesController : BaseController
     {
         private readonly IDeletableEntityRepository<ApplicationUser> userRepository;
         private readonly UserManager<ApplicationUser> userManager;
+        private readonly IServiceProvider serviceProvider;
 
         public UserRolesController(
             IDeletableEntityRepository<ApplicationUser> userRepository,
-            UserManager<ApplicationUser> userManager)
+            UserManager<ApplicationUser> userManager,
+            IServiceProvider serviceProvider)
         {
             this.userRepository = userRepository;
             this.userManager = userManager;
+            this.serviceProvider = serviceProvider;
         }
 
         [HttpGet]
@@ -40,11 +46,32 @@
                         CreatedOn = user.CreatedOn,
                         Email = user.Email,
                     }))
+                .Where(x => x.Role != "Administrator")
                 .ToList();
 
             return this.View(viewModel);
         }
 
+        public IActionResult Edit(string id)
+        {
+            //var user = await this.userManager.FindByIdAsync(id);
 
+            var roles = this.serviceProvider.
+                GetRequiredService<RoleManager<ApplicationRole>>()
+                .Roles
+                .Select(x => new SelectListItem
+                {
+                    Value = x.Id,
+                    Text = x.Name,
+                }).ToList();
+
+            var viewModel = new EditUserRolesViewModel
+            {
+                Id = id,
+                Roles = roles,
+            };
+
+            return this.View(viewModel);
+        }
     }
 }
