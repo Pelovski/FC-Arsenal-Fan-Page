b733a3663db0ffe9f9ca033e20d6bc408d883f65
Pavel Pelovski <git.pelovski@gmail.com>
2024-04-10 18:01:13 +0300

    Add to Cart implemented / working on the cart view

--- Services/FCArsenalFanPage.Services/OrderService.cs
+++ Services/FCArsenalFanPage.Services/OrderService.cs
@@ -1,6 +1,9 @@
 ﻿namespace FCArsenalFanPage.Services
 {
-    using System.Threading.Tasks;
+    using System.Collections.Generic;
+	using System.Linq;
+	using System.Security.Cryptography.X509Certificates;
+	using System.Threading.Tasks;
 
     using FCArsenalFanPage.Data.Common.Repositories;
     using FCArsenalFanPage.Data.Models;
@@ -36,6 +39,21 @@
 
             await this.orderRepository.AddAsync(order);
             await this.orderRepository.SaveChangesAsync();
+        }
+
+        public IEnumerable<OrdersInListViewModel> GetAll()
+        {
+            return this.orderRepository.AllAsNoTracking()
+                .Select(x => new OrdersInListViewModel
+                {
+                    Id = x.Id,
+                    ProductName = x.Product.Name,
+                    Quantity = x.Quantity,
+                    Price = x.Product.Price,
+                    Status = x.Status.Name,
+                    ImageUrl = x.Product.Image.RemoteImageUrl ?? "/Images/Products/"
+                    + x.Product.Image.Id + "." + x.Product.Image.Extension,
+                });
         }
     }
 }
---
+++ Web/FCArsenalFanPage.Web.ViewModels/Orders/OrdersInListViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace FCArsenalFanPage.Web.ViewModels.Orders
+{
+    public class OrdersInListViewModel
+    {
+        public string Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Status { get; set; }
+    }
+}
