46088dc80c91cbd019aaa6f7c31e5169e44256d0
Pavel Pelovski <git.pelovski@gmail.com>
2024-07-04 20:20:44 +0300

    Database migration added

--- FCArsenalFanPage.Common/obj/Debug/net7.0/FCArsenalFanPage.Common.AssemblyInfo.cs
+++ FCArsenalFanPage.Common/obj/Debug/net7.0/FCArsenalFanPage.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FCArsenalFanPage.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b0b52d97c6c59de9e232267a55e5b1a1e2eb3a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472f73aec255572466e16fb77ba6e6fe6a5a83c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FCArsenalFanPage.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FCArsenalFanPage.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
