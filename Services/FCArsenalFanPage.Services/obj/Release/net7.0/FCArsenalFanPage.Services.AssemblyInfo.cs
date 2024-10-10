c0b8fa8419d955f38cbbf5af70fe60cc7f3356af
Pavel Pelovski <git.pelovski@gmail.com>
2024-10-10 20:49:40 +0300

    Add active class toggle to navigation links

--- Services/FCArsenalFanPage.Services/obj/Release/net7.0/FCArsenalFanPage.Services.AssemblyInfo.cs
+++ Services/FCArsenalFanPage.Services/obj/Release/net7.0/FCArsenalFanPage.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FCArsenalFanPage.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98182718de9f4e82f5ece1e8290c9aae71e3d075")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10078740654e8a80507aa3522897fb5be92d30f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FCArsenalFanPage.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FCArsenalFanPage.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
